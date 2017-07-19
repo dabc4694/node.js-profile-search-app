5c66402c37aed887773d5d8349b51cb0f58c8eec
dabc4694 <darshan.shah130@gmail.com>
2017-07-19 15:02:07 -0500

    Start of App

---
+++ node.js-profile-search-app/project-files/app.js
@@ -0,0 +1,12 @@
+//Problem: using the username we have to print out  the total badges and js points
+//Solution: print out the username and total badges and points earned
+
+//print the output
+function printMessage(username, badges, points){
+
+    console.log("The username is: " + username);
+    console.log("Total badge(s) : " + badges);
+    console.log("Points earned in Javascript : " + points);
+}
+
+printMessage("Darshan", 300, 20000);
