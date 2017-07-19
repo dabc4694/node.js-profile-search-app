2814b30dd56819d9c7521e414f49cf5d060da3da
dabc4694 <darshan.shah130@gmail.com>
2017-07-19 15:34:56 -0500

    Printing the desired output of a single user

--- node.js-profile-search-app/project-files/app.js
+++ node.js-profile-search-app/project-files/app.js
@@ -6,10 +6,9 @@ const username = "darshanshah2";
 //print the output
 function printMessage(username, badges, points){
 
-    console.log("The username is: " + username);
-    console.log()
-    console.log("Total badge(s) : " + badges);
-    console.log("Points earned in Javascript : " + points);
+    console.log("\nThe username is: " + username + "\n");
+    console.log("Total badge(s) : " + badges + "\n");
+    console.log("Points earned in Javascript : " + points + "\n");
 }
 
 //printMessage("Darshan", 300, 20000);
@@ -23,7 +22,10 @@ https.get(`https://teamtreehouse.com/${username}.json`, response => {
 
     response.on('end', () => {
         const profile = JSON.parse(body);
-        console.log(profile.badges.length);
-        console.log(profile.points.JavaScript);
+        // console.log(username);
+        // console.log(profile.badges.length);
+        // console.log(profile.points.JavaScript);
+
+        printMessage(username, profile.badges.length, profile.points.JavaScript);
     });
 });
